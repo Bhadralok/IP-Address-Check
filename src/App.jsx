89983224f98ce3fda5f1123ea384f15d94910d4d
Bhadralok <gr8prinizini338@gmail.com>
2024-12-01 12:17:00 +0100

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+export default function App() {
+  const [ipaddress, setIpaddress] = useState(null);
+  const [error, setError] = useState(null);
+  const url = "https://api.aruljohn.com/ip/json";
+  async function getMovie() {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      setIpaddress(data.ip);
+    } catch (error) {
+      setError(`Error : ${error.message}`);
+    }
+  }
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <>
+      {error ? (
+        <p>Error fetching data</p>
+      ) : (
+        <div className="h-[80vh] w-[80vw] flex items-center justify-center main">
+          <div className="flex flex-col justify-center m-0 items-center">
+            <h1 className="text-white">Your IP address</h1>
+            <h2 className="ipaddress py-[0.7rem] px-[2rem]  font-bold text-white text-base sm:text-xl md:text-xl lg:text-5xl flex justify-center items-center rounded-full bg-black">
+              {ipaddress}
+            </h2>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
